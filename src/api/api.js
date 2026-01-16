f60fbc74683a46e094300f2c243e90c0fc89b8fe
mateuspossera-del <mateuspossera@gmail.com>
2026-01-16 00:47:22 -0300

    api usa VITE_API_URL

--- src/api/api.js
+++ src/api/api.js
@@ -2,9 +2,10 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 20000,
 });
+
 
 // ✅ garante que o header exista
 api.interceptors.request.use(
